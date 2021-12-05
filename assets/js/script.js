c57942ea9155460076c2051ad3b843daf11f8fcc
Chewbakkah <chewbakkah@gmail.com>
2021-12-05 17:46:07 -0600

    fixed error causing code to stop at broken answer loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 var startBtnEl = document.querySelector("#start-btn");
+var nextBtnEl = document.querySelector("#next-btn");
 var instructionsEl = document.querySelector("#instructions");
 var questionsEl = document.querySelector("#question-container");
 var questionEl = document.querySelector("#question");
 var answerEl = document.querySelector("#answer-buttons");
+var correctAnswerEl = document.querySelector("#correct-prompt");
+var wrongAnswerEl = document.querySelector("#wrong-prompt");
+var returnCorrectEl = document.querySelector("#return-correct");
+var removeLaterEl = document.querySelector("#remove-later");
 // x = question from questionbank
 var x = 0;
 // y = answers from questionbank
@@ -24,8 +29,8 @@ var questionBank = [
     available: true,
     question: "Test Question 2 Answer B",
     answers: [
-      { text: "a", correct: false },
-      { text: "b", correct: true },
+      { text: "d", correct: false },
+      { text: "e", correct: true },
     ],
   },
   {
@@ -33,8 +38,8 @@ var questionBank = [
     available: true,
     question: "Test Question 3 Answer B",
     answers: [
-      { text: "a", correct: false },
-      { text: "b", correct: true },
+      { text: "f", correct: false },
+      { text: "g", correct: true },
     ],
   },
   {
@@ -42,8 +47,8 @@ var questionBank = [
     available: true,
     question: "Test Question 4 Answer B",
     answers: [
-      { text: "a", correct: false },
-      { text: "b", correct: true },
+      { text: "h", correct: false },
+      { text: "i", correct: true },
     ],
   },
   {
@@ -51,8 +56,8 @@ var questionBank = [
     available: true,
     question: "Test Question 5 Answer B",
     answers: [
-      { text: "a", correct: false },
-      { text: "b", correct: true },
+      { text: "j", correct: false },
+      { text: "k", correct: true },
     ],
   },
   {
@@ -60,8 +65,8 @@ var questionBank = [
     available: true,
     question: "Test Question 6 Answer B",
     answers: [
-      { text: "a", correct: false },
-      { text: "b", correct: true },
+      { text: "l", correct: false },
+      { text: "m", correct: true },
     ],
   },
   {
@@ -69,8 +74,8 @@ var questionBank = [
     available: true,
     question: "Test Question 7 Answer B",
     answers: [
-      { text: "a", correct: false },
-      { text: "b", correct: true },
+      { text: "n", correct: false },
+      { text: "o", correct: true },
     ],
   },
   {
@@ -78,8 +83,8 @@ var questionBank = [
     available: true,
     question: "Test Question 8 Answer B",
     answers: [
-      { text: "a", correct: false },
-      { text: "b", correct: true },
+      { text: "p", correct: false },
+      { text: "q", correct: true },
     ],
   },
   {
@@ -87,8 +92,8 @@ var questionBank = [
     available: true,
     question: "Test Question 9 Answer B",
     answers: [
-      { text: "a", correct: false },
-      { text: "b", correct: true },
+      { text: "r", correct: false },
+      { text: "s", correct: true },
     ],
   },
   {
@@ -96,8 +101,8 @@ var questionBank = [
     available: true,
     question: "Test Question 10 Answer B",
     answers: [
-      { text: "a", correct: false },
-      { text: "b", correct: true },
+      { text: "t", correct: false },
+      { text: "u", correct: true },
     ],
   },
 ];
@@ -117,45 +122,70 @@ function countdown() {
     } else {
       timerEl.textContent = "";
       clearInterval(timeInterval);
-      //put game ending function here
+      endScreen();
     }
   }, 1000);
 }
 
+var endScreen = function () {
+  // if(questionsEl.classList != "hidden"){
+  // questionsEl.classList.add("hidden")
+  // }
+  // if(El.classList != "hidden"){
+  //     questionsEl.classList.add("hidden")
+  //     }
+
+  console.log("game over");
+};
+
 var randomQuestionSelector = function () {
   z = Math.floor(Math.random() * 10);
-  if (questionBank[z].available === false) {
-    randomQuestionSelector();
-  } else {
+  if (questionBank[z].available === true) {
     x = z;
+  } else if (questionBank[z].available === false) {
+    randomQuestionSelector;
+  } else {
+    endScreen();
   }
 };
 
 var answerBtnClicked = function (event) {
-    let selectedBtn = event.target
-    let correctBtn = selectedBtn.dataset.correct
-    if (correctBtn === "true"){
-        console.log("you clicked correct answer");
-    }else{
-        console.log("you clicked wrong answer");
-    }
+  let selectedBtn = event.target;
+  let correctBtn = selectedBtn.dataset.correct;
+  if (correctBtn === "true") {
+    console.log("you clicked correct answer");
+    correctAnswerEl.classList.remove("hidden");
+    highScore++;
+  } else {
+    console.log("you clicked wrong answer");
+    wrongAnswerEl.classList.remove("hidden");
+  }
+  questionEl.classList.add("hidden");
+  answerEl.classList.add("hidden");
+  nextBtnEl.classList.remove("hidden");
 };
 
 var populateAnswers = function () {
-  let buttonEl = document.createElement("button");
-  buttonEl.className = "btn answer-btn";
-  let buttonElText = document.createTextNode(questionBank[x].answers[y].text);
-  if (questionBank[x].answers[y].correct === true) {
-    buttonEl.dataset.correct = questionBank[x].answers[y].correct;
+  var n = questionBank[x].answers.length - 1;
+  for (i = 0; i < n; i++) {
+    console.log("button created");
+    let buttonEl = document.createElement("button");
+    buttonEl.className = "btn answer-btn";
+    let buttonElText = document.createTextNode(questionBank[x].answers[y].text);
+    if (questionBank[x].answers[y].correct === true) {
+      buttonEl.dataset.correct = questionBank[x].answers[y].correct;
+      returnCorrectEl.textContent = questionBank[x].answers[y].text;
+    }
+    buttonEl.addEventListener("click", answerBtnClicked);
+    buttonEl.appendChild(buttonElText);
+    answerEl.appendChild(buttonEl);
+    y++;
   }
-  buttonEl.addEventListener("click", answerBtnClicked);
-  buttonEl.appendChild(buttonElText);
-  answerEl.appendChild(buttonEl);
-  y++;
 };
 
 var populateQuestion = function () {
   let pEl = document.createElement("p");
+  pEl.id = "remove-later";
   let pElText = document.createTextNode(questionBank[x].question);
   pEl.appendChild(pElText);
   questionEl.appendChild(pEl);
@@ -167,7 +197,7 @@ var populateQuestion = function () {
   }
 };
 
-startBtnClicked = function () {
+var startBtnClicked = function () {
   console.log("you clicked start");
   startBtnEl.classList.add("hidden");
   instructionsEl.classList.add("hidden");
@@ -175,6 +205,34 @@ startBtnClicked = function () {
   randomQuestionSelector();
   populateQuestion();
   countdown();
+};
+
+var nextBtnClicked = function () {
+  console.log("you clicked next");
+  y = 0;
+  clearQuestion();
+  clearAnswers();
+  nextBtnEl.classList.add("hidden");
+  if (wrongAnswerEl.classList != "hidden") {
+    wrongAnswerEl.classList.add("hidden");
+  }
+  if (correctAnswerEl.classList != "hidden") {
+    correctAnswerEl.classList.add("hidden");
+  }
+  questionEl.classList.remove("hidden");
+  answerEl.classList.remove("hidden");
+  randomQuestionSelector();
+  populateQuestion();
+};
+
+var clearQuestion = function () {
+  var removeQuestion = document.getElementById("remove-later");
+  removeQuestion.remove();
+};
+
+var clearAnswers = function () {
+  var removeAnswers = document.getElementById("answer-buttons");
+  removeAnswers.innerHTML = "";
 };
 
 // //On start button click do the following:
@@ -199,3 +257,4 @@ startBtnClicked = function () {
 // give button to try again
 
 startBtnEl.addEventListener("click", startBtnClicked);
+nextBtnEl.addEventListener("click", nextBtnClicked);
